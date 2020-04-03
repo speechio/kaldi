7d0db3660402fef9797c41f1bd918a169030b8dd
dophist <jerry.ejwt@qq.com>
2020-04-03 11:48:42 +0800

    update show_results.sh to deal with multiple test sets

--- egs/cn_env/s0/local/show_results.sh
+++ egs/cn_env/s0/local/show_results.sh
@@ -1,17 +1,12 @@
 # !/bin/bash
-test_sets="dev test"
-for s in $test_sets; do
-  echo "----- $s -----:"
-
+echo "-------------------- Show Results --------------------"
   # gmm models
-  for m in exp/*/decode_${s}; 
+  for m in exp/*/decode_*test*; 
     do [ -d $m ] && grep WER $m/cer_* | utils/best_wer.sh; 
   done 2>/dev/null
 
   # chain models
-  for m in exp/*/*/decode_${s};
+  for m in exp/*/*/decode_*test*;
     do [ -d $m ] && grep WER $m/cer_* | utils/best_wer.sh; 
   done 2>/dev/null
-
-  echo ""
-done 2>/dev/null
+echo "------------------------------------------------------"
