cb7a6ad27f7055086d974d40d0beaaf0a86a5410
dophist <jerry.ejwt@qq.com>
2020-05-27 17:38:05 +0800

    first draft of add_noise tool

---
+++ egs/cn_env/s0/local/add_noise.sh
@@ -0,0 +1,41 @@
+#!/bin/sh
+
+. ./path.sh
+. ./utils/parse_options.sh
+
+if [ $# -ne 5 ]; then
+    echo "add_noise.sh <wav.list> <noise.list> <snr_lower> <snr_upper> <working_dir>"
+    echo "  --normalize-output false(default)"
+    echo "  --background-mode true(default)"
+    echo "  --random-noise-position true(default)"
+    echo "e.g: add_noise.sh wav.list noise.list 0 10 wdir"
+    exit 1;
+fi
+
+normalize_output=false
+background_mode=true
+random_noise_position=true
+
+wav_list=$1
+noise_list=$2
+snr_lower=$3
+snr_upper=$4
+dir=$5
+
+mkdir -p $dir
+
+snr_seq=`seq ${snr_lower} ${snr_upper}`
+
+for f in `cat $wav_list`; do
+    noise=`shuf $noise_list | head -n 1`
+    snr=`seq ${snr_lower} ${snr_upper} | shuf | head -n 1`
+    iname=`basename $f`
+    oname=SNR${snr}_$iname
+    echo "adding noise: $noise to audio: $f with snr: $snr, destination:$dir/$oname"
+    wav-reverberate \
+        --background-mode=$background_mode --normalize-output=$normalize_output \
+        --additive-signals="$noise" --snrs="$snr" --random-noise-position=$random_noise_position \
+        --start-times="0" \
+        $f $dir/$oname
+done
+
