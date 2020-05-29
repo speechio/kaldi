5537037228a9b8986d5fb79020f16462c3f5bf8c
dophist <jerry.ejwt@qq.com>
2020-05-29 21:07:44 +0800

    parallel processing of add-noise tool, i/o bottlenecked

--- egs/cn_env/s0/local/add_noise.sh
+++ egs/cn_env/s0/local/add_noise.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 
+nj=1
 normalize_output=false
 background_mode=true
 random_noise_position=true
@@ -11,6 +12,7 @@ append_snr=false
 
 if [ $# -ne 5 ]; then
     echo "add_noise.sh <wav.list/wav.scp> <noise.list> <snr_lower> <snr_upper> <dest_dir>"
+    echo "  --nj 1(default)"
     echo "  --append-snr false(default)"
     echo "  --append-suffix ''(default)"
     echo "  --normalize-output false(default)"
@@ -33,39 +35,49 @@ NF=`awk '{print NF}' $wavs | sort -nu | tail -n 1`
 mkdir -p $dir
 opts="--background-mode=$background_mode --normalize-output=$normalize_output --random-noise-position=$random_noise_position"
 
-while read l; do
-    if [ $NF -eq 1 ]; then 
-        # wav.list
-        f=$l
-        filename=`basename $f`
-        key="${filename%.*}"
-        ext="${filename##*.}"
-    elif [ $NF -eq 2 ]; then 
-        # wav.scp
-        key=`echo $l | awk '{print $1}'`
-        f=`echo $l | awk '{print $2}'`
-        filename=`basename $f`
-        ext="${filename##*.}"
-    else
-        echo "Error: unknown format, need wav.list or wav.scp"
-        exit 1
-    fi
+wdir=`mktemp -d add_noise_$(date +%Y-%m-%d)_XXX`
+echo $wdir
+#trap "rm $wdir" EXIT
 
-    if [ ! -z "$append_suffix" ]; then
-        key="${key}__${append_suffix}"
-    fi
+split -n l/${nj} $wavs $wdir/job_
+
+for job in `ls $wdir/job_*`; do
+    while read l; do
+        if [ $NF -eq 1 ]; then 
+            # wav.list
+            f=$l
+            filename=`basename $f`
+            key="${filename%.*}"
+            ext="${filename##*.}"
+        elif [ $NF -eq 2 ]; then 
+            # wav.scp
+            key=`echo $l | awk '{print $1}'`
+            f=`echo $l | awk '{print $2}'`
+            filename=`basename $f`
+            ext="${filename##*.}"
+        else
+            echo "Error: unknown format, need wav.list or wav.scp"
+            exit 1
+        fi
     
-    noise=`shuf $noise_list | head -n 1`
-    snr=`seq ${snr_lower} ${snr_upper} | shuf | head -n 1`
-
-    if [ "$append_snr" = "true" ]; then
-        snr_str="SNR${snr}dB"
-        key="${key}__${snr_str}"
-    fi
-
-    o=${key}.${ext}
-    echo -e "$n\t${key}\t$dir/$o\tsnr:$snr\tnoise:$noise"
-    wav-reverberate $opts --additive-signals="$noise" --snrs="$snr" --start-times=0 $f $dir/$o
-    n=$((n+1))
-done < $wavs
+        if [ ! -z "$append_suffix" ]; then
+            key="${key}__${append_suffix}"
+        fi
+        
+        noise=`shuf $noise_list | head -n 1`
+        snr=`seq ${snr_lower} ${snr_upper} | shuf | head -n 1`
+    
+        if [ "$append_snr" = "true" ]; then
+            snr_str="SNR${snr}dB"
+            key="${key}__${snr_str}"
+        fi
+    
+        o=${key}.${ext}
+        wav-reverberate $opts --additive-signals="$noise" --snrs="$snr" --start-times=0 $f $dir/$o
+        echo -e "DONE=$n\tKEY=${key}\tFILE=$dir/$o\tRAW=$f\tSNR=$snr\tNOISE=$noise"
+        n=$((n+1))
+    done < $job >& $wdir/log.`basename $job` &
+done
+wait
+echo "`basename $0`: Done."
 
