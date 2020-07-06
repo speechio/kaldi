847f61831504cf8d520cefecfa73d34aa0ec4505
dophist <jerry.ejwt@qq.com>
2020-07-06 18:43:09 +0800

    fix typo

--- egs/cn_env/s0/local/make_rescore_graph.sh
+++ egs/cn_env/s0/local/make_rescore_graph.sh
@@ -2,8 +2,8 @@ mode=
 . utils/parse_options.sh
 
 if [ $# -ne 3 ]; then
-    echo "arpa2fst.sh --mode=fst   <G.arpa> <words.txt> <Gr.fst>"
-    echo "arpa2fst.sh --mode=carpa <G.arpa> <words.txt> <G.carpa>"
+    echo "arpa2fst.sh --mode fst   <G.arpa> <words.txt> <Gr.fst>"
+    echo "arpa2fst.sh --mode carpa <G.arpa> <words.txt> <G.carpa>"
     exit 1;
 fi
 
@@ -23,8 +23,8 @@ elif [ $mode = "carpa" ]; then
     unk=`grep '<unk>' $vocab | awk '{print $2}'`
     bos=`grep '<s>'   $vocab | awk '{print $2}'`
     eos=`grep '</s>'  $vocab | awk '{print $2}'`
-    #arpa-to-const-arpa --bos-symbol=$bos --eos-symbol=$eos --unk-symbol=$unk "cat $arpa | utils/map_arpa_lm.pl $vocab|" $output
-    arpa-to-const-arpa --bos-symbol=$bos --eos-symbol=$eos --unk-symbol=$unk $arpa $output
+    arpa-to-const-arpa --bos-symbol=$bos --eos-symbol=$eos --unk-symbol=$unk "cat $arpa | utils/map_arpa_lm.pl $vocab|" $output
+    #arpa-to-const-arpa --bos-symbol=$bos --eos-symbol=$eos --unk-symbol=$unk $arpa $output
 fi
 
 echo "Rescore graph done."
