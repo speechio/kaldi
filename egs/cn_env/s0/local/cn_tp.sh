e0dbcfbb3002fc47b6305a20ff95816902d2ab53
dophist <jerry.ejwt@qq.com>
2020-07-10 17:11:40 +0800

    add --has_key option to cn_tp.py tool

--- egs/cn_env/s0/local/cn_tp.sh
+++ egs/cn_env/s0/local/cn_tp.sh
@@ -1,19 +1,25 @@
 #!/bin/sh
 
 nj=1
+has_key=false
 
 . ./path.sh
 . ./utils/parse_options.sh
 
 # CN Text Processing
 if [ $# -ne 3 ]; then
-    echo "cn_tp.sh [--nj <nj>] <vocab.txt> <text.txt> <working_dir>"
+    echo "cn_tp.sh [--nj <nj>] [--has-key true/false]  <vocab.txt> <text.txt> <working_dir>"
     exit 1;
 fi
 
 vocab=$1
 text=$2
 dir=$3
+
+opts=""
+if [ $has_key ]; then
+    opts="--has_key"
+fi
 
 name=`basename $text .txt`
 new_text=${name}_tn_ws.txt
@@ -24,25 +30,25 @@ cat $vocab | grep -v '<eps>' | grep -v '#0' | grep -v '<unk>' | grep -v '<UNK>' 
 
 if [ $nj -eq 1 ]; then
     echo "TN..."
-    local/cn_tn.py --to_upper $text $dir/${name}_tn
+    local/cn_tn.py $opts --to_upper $text $dir/${name}_tn
     echo "TN done."
 
     echo "WS..."
-    local/cn_ws.py $dir/jieba.vocab $dir/${name}_tn $dir/${new_text}
+    local/cn_ws.py $opts $dir/jieba.vocab $dir/${name}_tn $dir/${new_text}
     echo "WS done."
 else
     # parallel tn & ws
     echo "TN..."
     split -n l/${nj} $text $dir/${name}_
     for f in `ls $dir/${name}_*`; do
-        local/cn_tn.py --to_upper $f ${f}_tn >& ${f}_tn.log &
+        local/cn_tn.py $opts --to_upper $f ${f}_tn >& ${f}_tn.log &
     done
     wait
     echo "TN done."
 
     echo "WS..."
     for f in `ls $dir/${name}_*_tn`; do
-        local/cn_ws.py $dir/jieba.vocab $f ${f}_ws >& ${f}_ws.log &
+        local/cn_ws.py $opts $dir/jieba.vocab $f ${f}_ws >& ${f}_ws.log &
     done
     wait
     echo "WS done."
