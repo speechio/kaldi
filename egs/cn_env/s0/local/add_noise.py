f81c0effb88d47611037e4e6181e9f037944b3d7
dophist <jerry.ejwt@qq.com>
2020-07-31 15:05:06 +0800

    remove spk2utt, not necessary

--- egs/cn_env/s0/local/add_noise.py
+++ egs/cn_env/s0/local/add_noise.py
@@ -13,8 +13,10 @@ parser.add_argument("snr_upper", type=int, default=20, help="")
 parser.add_argument("odir", type=str, default="", help="")
 
 parser.add_argument("--list", type=str, default="", help="wav.list")
+
 parser.add_argument("--scp", type=str, default="", help="wav.scp")
 parser.add_argument("--trans", type=str, default="", help="trans.txt")
+parser.add_argument("--utt2spk", type=str, default="", help="utt2spk")
 
 parser.add_argument("--normalize_output", type=str, default="false", help="true / false")
 parser.add_argument("--background_mode", type=str, default="true", help="true / false")
@@ -50,7 +52,7 @@ with open(args.noise_list, 'r') as f:
     l = l.strip()
     if l != "":
       noise_list.append(l)
-sys.stderr.write("Noise Library Size:{}\n".format(len(noise_list)))
+sys.stderr.write("Loaded Noise Library # wavs:{}\n".format(len(noise_list)))
 
 snr_list = list(range(args.snr_lower, args.snr_upper))
 sys.stderr.write("Add noise SNR range:[{},{}]dB\n".format(snr_list[0], snr_list[-1]))
@@ -59,6 +61,7 @@ keys = []
 wavs = {}
 
 if args.list != '':
+  sys.stderr.write('Adding noise to wav list...\n')
   # load list
   assert(os.path.isfile(args.list))
   with open(args.list, 'r') as wav_list:
@@ -70,6 +73,8 @@ if args.list != '':
         keys.append(key)
         assert(wavs.get(key) == None) # check duplicated key
         wavs[key] = f
+  sys.stderr.write('# of wavs:{}\n'.format(len(wavs)))
+
   # add noise
   for i in range(len(keys)):
     key = keys[i]
@@ -85,22 +90,27 @@ if args.list != '':
     if (args.key_append_string != ''):
       new_key += '__{}'.format(args.key_append_string)
     
-    cmd = ADD_NOISE_TOOL + opts + ' additive-signal={} '.format(noise) + ' --snr={} '.format(snr) + wav + ' ' + os.path.join(args.odir, new_key+'.wav')
-    # os.system(cmd)
+    new_wav = os.path.join(args.odir, new_key+'.wav')
+
+    sys.stderr.write('{}  key={}  f={}  noise={}  snr={}dB  new_key={}  new_f={}\n'.format(i, key, wav, noise, snr, new_key, new_wav))
+    cmd = ADD_NOISE_TOOL + opts + ' additive-signal={} '.format(noise) + ' --snr={} '.format(snr) + wav + ' ' + new_wav
+    #os.system(cmd)
 
 elif args.scp != "":
+  sys.stderr.write('Adding noise to wav scp...\n')
   # load scp
   assert(os.path.isfile(args.scp))
   with open(args.scp, 'r') as scp_list:
     for l in scp_list:
       l = l.strip()
-      if l != "":
+      if (l != ''):
         key, f = l.split(maxsplit=1)
         assert(len(keys) == len(wavs))
         keys.append(key)
         assert(wavs.get(key) == None) # check duplicated key
         wavs[key] = f
-  
+  sys.stderr.write('# of wavs loaded:{}\n'.format(len(wavs)))
+
   # load trans
   trans = {}
   if args.trans != '':
@@ -108,12 +118,26 @@ elif args.scp != "":
     with codecs.open(args.trans, 'r', 'utf8') as trans_list:
       for l in trans_list:
         l = l.strip()
-        if l != '':
+        if (l != ''):
           key, text = l.split(maxsplit=1)
           assert(trans.get(key) == None)
           trans[key] = text
+  sys.stderr.write('# of trans loaded:{}\n'.format(len(trans)))
 
-  # add noise and write modified trans
+  # load utt2spk
+  utt2spk = {}
+  if args.utt2spk != '':
+    assert(os.path.isfile(args.utt2spk))
+    with open(args.utt2spk, 'r') as utt2spk_file:
+      for l in utt2spk_file:
+        l = l.strip()
+        if (l != ''):
+          key, spk = l.split(maxsplit=1)
+          assert(utt2spk.get(key) == None)
+          utt2spk[key] = spk
+  sys.stderr.write('# of utt2spk loaded:{}\n'.format(len(utt2spk)))
+
+  # add noise
   os.mkdir(args.odir)
   os.mkdir(os.path.join(args.odir, 'wav'))
 
@@ -121,7 +145,6 @@ elif args.scp != "":
   if args.trans != '':
     otrans = codecs.open(os.path.join(args.odir, 'trans.txt'), 'w+', 'utf8')
   outt2spk = open(os.path.join(args.odir, 'utt2spk'), 'w+')
-  ospk2utt = open(os.path.join(args.odir, 'spk2utt'), 'w+')
 
   for i in range(len(keys)):
     key = keys[i]
@@ -138,23 +161,31 @@ elif args.scp != "":
       new_key += '__{}'.format(args.key_append_string)
 
     new_wav = os.path.join(args.odir, 'wav', new_key+'.wav')
-    cmd = ADD_NOISE_TOOL + opts + ' additive-signal={} '.format(noise) + ' --snr={} '.format(snr) + wav + ' ' + new_wav
-    sys.stderr.write('{} ==> {}\n'.format(i, cmd))
-    #os.system()
 
+    sys.stderr.write('{}  key={}  f={}  noise={}  snr={}dB  new_key={}  new_f={}\n'.format(i, key, wav, noise, snr, new_key, new_wav))
+    cmd = ADD_NOISE_TOOL + opts + ' additive-signal={} '.format(noise) + ' --snr={} '.format(snr) + wav + ' ' + new_wav
+    #os.system(cmd)
+
+    # write new wav.scp
     oscp.write('{}\t{}\n'.format(new_key, os.path.join('wav', new_key+'.wav')))
+    
+    # write new trans.txt
     if (args.trans != ''):
       text = trans.get(key)
       assert(text != None) # missing trans?
       otrans.write('{}\t{}\n'.format(new_key, text))
-    outt2spk.write('{}\t{}\n'.format(new_key, new_key))
-    ospk2utt.write('{}\t{}\n'.format(new_key, new_key))
+    
+    # write new utt2spk
+    spk = utt2spk.get(key)
+    if spk != None:
+      outt2spk.write('{}\t{}\n'.format(new_key, spk))
+    else:
+      outt2spk.write('{}\t{}\n'.format(new_key, key))
   
   oscp.close()
   if (args.trans != ''):
     otrans.close()
   outt2spk.close()
-  ospk2utt.close()
 
 else:
   sys.stderr.write("not list/scp?")
