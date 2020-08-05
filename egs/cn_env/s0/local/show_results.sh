939a900bb3efbfb30f480e8842a07ec15f9a664d
dophist <jerry.ejwt@qq.com>
2020-08-05 17:11:35 +0800

    add dev set result printing to show_results.sh

--- egs/cn_env/s0/local/show_results.sh
+++ egs/cn_env/s0/local/show_results.sh
@@ -1,12 +1,16 @@
 # !/bin/bash
 echo "-------------------- Show Results --------------------"
   # gmm models
-  for m in exp/*/decode_*test*; 
+for x in dev test; do
+  for m in exp/*/decode_*$x*; 
     do [ -d $m ] && grep WER $m/cer_* | utils/best_wer.sh; 
   done 2>/dev/null
 
   # chain models
-  for m in exp/*/*/decode_*test*;
+  for m in exp/*/*/decode_*$x*;
     do [ -d $m ] && grep WER $m/cer_* | utils/best_wer.sh; 
   done 2>/dev/null
+
+  echo -e "\n"
+done
 echo "------------------------------------------------------"
